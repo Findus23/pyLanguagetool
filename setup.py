0fbcd05eb21e9ac00f49efe1d42213d615bfbf39
Lukas Winkler <l.winkler23@mailbox.org>
2017-01-20 11:48:02 +0100

    0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyLanguagetool',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     url='https://github.com/Findus23/pylanguagetool',
     license='MIT',
