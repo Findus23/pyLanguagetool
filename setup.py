b5ef90b6a037a304a7baff554cb62283bc0da664
Lukas Winkler <l.winkler23@mailbox.org>
2017-09-06 19:52:56 +0200

    release 0.5.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyLanguagetool',
-    version='0.4.3',
+    version='0.5.0',
     packages=find_packages(),
     url='https://github.com/Findus23/pylanguagetool',
     license='MIT',
